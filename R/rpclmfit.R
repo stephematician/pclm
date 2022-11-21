645ad96c0098e75575c1f96c7be1fdfa37ee9b73
Stephen Wade <Stephen.Wade@nswcc.org.au>
2022-11-21 14:15:48 +1100

    Reimplement rpclmfit
    Improve speed and stability of rpclmfit

--- R/rpclmfit.R
+++ R/rpclmfit.R
@@ -1,65 +1,68 @@
 #' Randomly draw samples from fitted PCLM
 #'
-#' Scales reasonably well (linear?) with
-#' \itemize{
-#'   \item number of observations
-#'   \item number of unique (observed) intervals
-#'   \item number of samples per interval
-#' }
+#' Generate a sample for each observation (or otherwise) of a variable taken
+#' from an interval of a PCLM.
 #'
-#' Takes about 1 second on a 2016 desktop, Windows 7, for
-#' \itemize{
-#'   \item 100000 observations
-#'   \item 2500 unique intervals
-#'   \item 5 samples per interval
-#' }
+#' Define any interval of bins (not necessarily the same as the original
+#' groupings) for each observation and then draw a bin from the PCLM using the
+#' probabilities obtained from `pclmfit`.
 #'
-#' Cannot handle PCLMs with more than \code{sqrt(.Machine$max.integer)} number
-#' of bins due to a hashing mechanism used
+#' The flexibility to define any interval does come at a small cost in terms of
+#' performance. This is most useful when incorporating some kind of censoring
+#' into a prediction that provides additional information to narrow down the
+#' range of possible bins. A simple example might be when 'age at survey' is
+#' grouped, but 'age at event' is not, and therefore the participant must be at
+#' least the latter age (in bins).
 #'
-#' @param D A two-column data.frame of integers, each row represents an
-#'          observation of an interval of bins, columns correspond to lower and
-#'          upper bins of the interval (inclusive)
-#' @param pclm_fit A list as returned by pclmfit
-#' @param n_sample Number of samples to draw for each observation
-#' @return Matrix of drawn bins
+#' @param D a two-column data.frame (or matrix) of integers, each row represents
+#'   an observation of an interval of bins, columns correspond to lower and
+#'   upper bins of the interval (inclusive)
+#' @param pclm_fit a list as returned by pclmfit
+#' @param n_sample number of samples to draw for each observation
+#' @return matrix of drawn bins with one row per observation, and columns
+#'   correspond to samples
 #'
 #' @export
+#' @md
 rpclmfit <- function(D, pclm_fit, n_sample=1L) {
-    
+
     n_bin <- length(pclm_fit$gamma)
 
-    # Check hashing scheme limitation
-    stopifnot(n_bin < sqrt(.Machine$integer.max))
-
-    hash_bin_range <- function(D_)
-        (D_[,1] - 1L) + n_bin * (D_[,2] - 1L)
-
-    unhash_bin_range <- function(x)
-        1L + ((x %% n_bin)):(x %/% n_bin)
-
-    # Check interval data are integer indices
-    stopifnot(all(sapply(D, is.integer)))
-    stopifnot(all(D[,1:2] >= 1L & D[,1:2] <= n_bin))
-    stopifnot(all(D[,1L] <= D[,2L]))
+  # check interval data are integer indices
+    stopifnot(all(apply(D, 2, is.integer)))
+    stopifnot(min(D[,1:2]) >= 1L && max(D[,1:2]) <= n_bin)
+    stopifnot(min(D[,2L] - D[,1L]) >= 0)
     if (!(is.integer(n_sample)))
         n_sample <- as.integer(n_sample)
 
-    h <- hash_bin_range(D)
-    h_table <- table(h) * n_sample
-    h_table_num <- as.integer(names(h_table))
+    bins <- matrix(NA_integer_, nrow=nrow(D), ncol=n_sample)
 
-    r <- matrix(NA_integer_, nrow=nrow(D), ncol=n_sample)
+    where_each <- split(1:nrow(D), D[,1L])
 
-    for (j in seq_len(length(h_table))) {
-        k <- unhash_bin_range(h_table_num[j])
-        r[h_table_num[j] == h,] <- sample(k,
-                                          size=h_table[j],
-                                          replace=T,
-                                          prob=pclm_fit$gamma[k])
+    for (start_j in seq_along(where_each)[sapply(where_each, length) > 0]) {
+
+      # find which rows have the current interval 'start', and get the number
+      # of draws required for each interval 'end'
+        where_j <- where_each[[start_j]]
+        max_end_j <- max(D[where_j,2L])
+        n_draw <- tabulate(D[where_j,2L], nbins=max_end_j)[start_j:max_end_j] *
+                      n_sample
+
+        for (delta_k in seq_along(n_draw)[n_draw != 0]) {
+          # for each [start, end] pair (i.e. index j,k), sample the values using
+          # the probability given by the PCLM
+            is_k <- D[where_j,2L] == (start_j - 1) + delta_k
+            bins[where_j[is_k],] <- (start_j - 1) + sample.int(
+                delta_k,
+                size=n_draw[delta_k],
+                replace=T,
+                prob=pclm_fit$gamma[(start_j - 1) + (1:delta_k)]
+            )
+        }
+
     }
 
-    r
+   bins 
 
 }
 
